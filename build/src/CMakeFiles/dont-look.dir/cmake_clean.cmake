b724b837f8a2907d42b6e43efdc3acecea6ff4bd
Archer Zhang <archer.xn@live.cn>
2016-12-03 11:10:11 -0500

    Init

---
+++ build/src/CMakeFiles/dont-look.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dont-look.dir/main.cpp.o"
+  "dont-look.pdb"
+  "dont-look"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dont-look.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Archer Zhang on 2016-11-19.
+//
+
+#include <iostream>
+#include <cmath>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char ** argv){
+
+
+    cout << "running" << endl;
+    Mat g;
+
+    cout << g << endl;
+
+
+    return 0;
+
+}
